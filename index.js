51a080f3db16b687387c03b99d5ea383d644c8da
WillianM19 <willianm1928@gmail.com>
2024-08-22 09:41:08 -0300

    feat: Realizando passos #7(Server) e #8(Client)

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+const app = express();
+const server = createServer(app);
+
+// open the database file
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+
+const io = new Server(server, {
+  connectionStateRecovery: {
+    maxDisconnectionDuration: 180000,
+  },
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+io.on('connection', (socket) => {
+  socket.on('chat message', async (msg, clientOffset, callback) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+    } catch (e) {
+      if (e.errno === 19 ) {
+        callback();
+      } else {
+      }
+      return;
+    }
+    io.emit('chat message', msg, result.lastID);
+    callback();
+  });
+
+  async function socketRecovered() {
+    if (!socket.recovered) {
+      // if the connection state recovery was not successful
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+      }
+    }
+  }
+  socketRecovered();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+server.listen(3000, () => console.log("running at port 3000"));
